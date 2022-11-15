67e597c1b17bc05d1f6098fc45c054834729e652
Bryan Chu <bryanchu10@gmail.com>
2022-11-15 22:24:32 +0800

    第一章圖桌面版完成

--- src/main.js
+++ src/main.js
@@ -1,11 +1,17 @@
 import { createApp } from 'vue';
+
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { TextPlugin } from 'gsap/TextPlugin';
+
 import App from './App.vue';
 import router from './router';
-
 import './assets/main.css';
+
+gsap.registerPlugin(ScrollTrigger, TextPlugin);
 
 const app = createApp(App);
 
 app.use(router);
-
+app.provide('$gsap', gsap);
 app.mount('#app');
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,3 +1,64 @@
+<template>
+  <div class="h-screen bg-[url('@/assets/images/town-rock.png')]">
+    <NavbarComponent />
+    <div class="banner container mx-auto bg-black pt-10 relative">
+      <h1
+        class="
+          border-[3px] rounded-[13px]
+          w-fit mx-auto font-cubic text-white md:border-[12px] md:rounded-[33px] border-white
+          px-3 relative"
+      >
+        <span
+          class="
+            text-[56px] leading-[65px]
+            md:text-[200px] md:leading-[233px]"
+        >
+          The F2E 4th
+        </span>
+        <span
+          class="
+            text-2xl leading-[28px]
+            md:text-[47px] md:leading-[47px] md:bg-black
+            md:absolute md:right-7 md:-bottom-1 md:translate-y-2/3"
+        >
+          互動式網頁設計
+        </span>
+      </h1>
+      <div class="absolute bottom-0 overflow-hidden">
+        <h3
+          class="attack-warning
+            font-cubic bg-white/70 text-center text-8xl py-7 text-r
+            absolute top-1/3 left-0 right-0 z-10"
+        >
+          F2E攻擊了你的村莊！
+        </h3>
+        <p class="font-cubic text-5xl text-white text-center absolute top-2/3 left-0 right-0">
+          請按空白鍵開始
+        </p>
+        <img
+          src="@/assets/images/castle.svg"
+          alt="castle"
+        >
+        <img
+          src="@/assets/images/fire.gif"
+          alt="fire"
+          class="absolute w-40 top-[9%] right-[16%]"
+        >
+        <img
+          src="@/assets/images/fire.gif"
+          alt="fire"
+          class="absolute w-48 top-[45%] left-[10%] -scale-x-100"
+        >
+        <img
+          src="@/assets/images/fire.gif"
+          alt="fire"
+          class="absolute w-48 -bottom-[6%] right-[13%]"
+        >
+      </div>
+    </div>
+  </div>
+</template>
+
 <script>
 import NavbarComponent from '@/components/NavbarComponent.vue';
 
@@ -5,14 +66,27 @@ export default {
   components: {
     NavbarComponent,
   },
+  inject: ['$gsap'],
+  mounted() {
+    this.$gsap.fromTo(
+      '.attack-warning',
+      {
+        color: 'rgb(156, 163, 175)',
+      },
+      {
+        color: 'rgb(248, 68, 68)',
+        duration: 0,
+        repeat: -1,
+        yoyo: true,
+        repeatDelay: 1,
+      },
+    );
+  },
 };
 </script>
 
-<template>
-  <div class="h-screen bg-[url('@/assets/images/town-rock.png')]">
-    <NavbarComponent />
-    <div class="container">
-      123
-    </div>
-  </div>
-</template>
+<style scoped>
+.banner {
+  height: calc(100vh - 80px);
+}
+</style>
