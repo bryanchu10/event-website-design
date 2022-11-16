03eaafdcf1fe475ac18585047a5fcf7f09a82d2e
Bryan Chu <bryanchu10@gmail.com>
2022-11-16 23:14:29 +0800

    桌面版與行動版互相干涉調整

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,56 +5,56 @@
       <h1
         class="
           border-[3px] rounded-[13px]
-          w-fit mx-auto font-cubic text-white md:border-[12px] md:rounded-[33px] border-white
+          w-fit mx-auto font-cubic text-white lg:border-[12px] lg:rounded-[33px] border-white
           px-3 relative"
       >
         <span
           class="
             block text-[56px] leading-[65px] text-center
-            md:text-[200px] md:leading-[233px]"
+            lg:text-[200px] lg:leading-[233px]"
         >
           The F2E 4th
         </span>
         <span
           class="
             block text-2xl leading-[28px] text-end
-            md:text-[47px] md:leading-[47px] md:bg-black
-            md:absolute md:right-7 md:-bottom-1 md:translate-y-2/3"
+            lg:text-[47px] lg:leading-[47px] lg:bg-black
+            lg:absolute lg:right-7 lg:-bottom-1 lg:translate-y-2/3"
         >
           互動式網頁設計
         </span>
       </h1>
-      <div class="absolute top-0 bottom-0 left-0 right-0 overflow-hidden">
+      <div class="absolute top-0 lg:top-auto bottom-0 left-0 right-0 overflow-hidden">
         <h3
           class="attack-warning
-            font-cubic bg-white/70 text-center text-3xl md:text-8xl py-3 md:py-7 text-r
+            font-cubic bg-white/70 text-center text-3xl lg:text-8xl py-3 lg:py-7 text-r
             absolute top-1/3 left-0 right-0 z-10"
         >
           F2E攻擊了你的村莊！
         </h3>
-        <p class="font-cubic text-white text-center absolute top-1/2 md:top-2/3 left-0 right-0">
-          <span class="text-2xl md:hidden">點擊螢幕開始</span>
-          <span class="hidden md:inline text-5xl">請按空白鍵開始</span>
+        <p class="font-cubic text-white text-center absolute top-1/2 lg:top-2/3 left-0 right-0">
+          <span class="text-2xl lg:hidden">點擊螢幕開始</span>
+          <span class="hidden lg:inline text-5xl">請按空白鍵開始</span>
         </p>
         <img
           src="@/assets/images/castle.svg"
           alt="castle"
-          class="absolute h-[291px] object-cover bottom-0"
+          class="absolute lg:static h-[291px] lg:h-auto object-cover bottom-0"
         >
         <img
           src="@/assets/images/fire.gif"
           alt="fire"
-          class="absolute w-20 bottom-1/4 right-[16%] md:w-40 md:top-[9%]"
+          class="absolute w-20 bottom-1/4 right-[16%] lg:w-40 lg:top-[9%]"
         >
         <img
           src="@/assets/images/fire.gif"
           alt="fire"
-          class="absolute w-24 md:w-48 top-3/4 left-0 -scale-x-100 md:top-[45%] md:left-[10%]"
+          class="absolute w-24 lg:w-48 top-3/4 left-0 -scale-x-100 lg:top-[45%] lg:left-[10%]"
         >
         <img
           src="@/assets/images/fire.gif"
           alt="fire"
-          class="absolute w-24 bottom-0 right-0 md:w-48 md:-bottom-[6%] md:right-[13%]"
+          class="absolute w-24 bottom-0 right-0 lg:w-48 lg:-bottom-[6%] lg:right-[13%]"
         >
       </div>
     </div>
